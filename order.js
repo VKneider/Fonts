54000e80363095d819f9de94c77cd940e3409e47
Victor Kneider <victorkneider@gmail.com>
2022-05-09 02:11:31 -0400

    feat: add proyecto

---
+++ order.js
@@ -0,0 +1,23 @@
+function returnText(){
+    let input = document.getElementById("userInput").value;
+    document.getElementById("input").innerHTML = input;
+    document.getElementById("input2").innerHTML = input;
+    document.getElementById("input3").innerHTML = input;
+    document.getElementById("input4").innerHTML = input;
+    document.getElementById("input5").innerHTML = input;
+    document.getElementById("input6").innerHTML = input;
+    document.getElementById("input7").innerHTML = input;
+    document.getElementById("input8").innerHTML = input;
+    document.getElementById("input9").innerHTML = input;
+    document.getElementById("input10").innerHTML = input;
+    document.getElementById("input11").innerHTML = input;
+    document.getElementById("input12").innerHTML = input;
+    document.getElementById("input13").innerHTML = input;
+    document.getElementById("input14").innerHTML = input;
+    document.getElementById("input15").innerHTML = input;
+    document.getElementById("input16").innerHTML = input;
+
+ 
+
+}
+
